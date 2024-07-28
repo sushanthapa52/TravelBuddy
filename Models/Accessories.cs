793334dec79cf798938a144ca7e013d78837487f
Satish Awal <satishawal94@gmail.com>
2024-07-27 22:06:54 -0400

    Create Accessories.cs
    Added Accessories model class to the project
    
    Summary:
    - Implemented the Accessories model class with attributes such as Tent,Sleeping_Bag,Camping_Stove,Cookware
    
    Details:
    - Created a new file `Accessories.cs` inside the `models` directory.
    - Defined six public fields: Tent, sleeping_bag, camping_stove, cookware, firstaid_kit

---
+++ Models/Accessories.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Models
+{
+    public class Accessories
+    {
+        public string Tent { get; set; }
+
+        public string Sleeping_Bag { get; set; }
+
+        public string Camping_Stove { get; set; }
+        public string Cookware { get; set; }
+
+        public string  FirstAid_Kit{ get; set; }
+    }
+}
