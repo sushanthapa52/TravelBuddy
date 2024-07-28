dc74b8ca449feb10e68d2036923d42637ad34742
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-07-27 21:59:56 -0400

    description added

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace TravelBuddy.Models
 {
+
+    /// <summary>
+    /// The Activity class contains a list of recreational activities that a user wants to do.
+    /// Each user can create multiple activities, including camping, swimming, and canoeing.
+    /// This class helps in organizing and managing various outdoor and leisure activities for users.
+    /// </summary>
     public class Activity
     {
         public string Camping { get; set; }
--- Models/Trip.cs
+++ Models/Trip.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace TravelBuddy.Models
 {
+
+    /// <summary>
+    /// The Trip class represents a trip planned by the user.
+    /// It includes a unique TripID, the name of the trip, and the category of the trip.
+    /// This class helps in organizing and categorizing various trips for better management.
+    /// </summary>
     public class Trip
     {
         public int TripID { get; set; }
