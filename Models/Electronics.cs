577705dddd616ff81cc117f7a55f757d08f6ed0e
Satish Awal <satishawal94@gmail.com>
2024-07-27 22:14:17 -0400

    Create Electronics.cs
    Added Electronics model class to the project
    
    Summary:
    - Implemented the Electronics model class with attributes: Camera, Headphones, Phone, Powerbank, and Headlamp.
    - Created a new file `Electronics.cs` inside the `models` directory.
    - This model represents various electronic items that a user might carry.

---
+++ Models/Electronics.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Models
+{
+    public class Electronics
+    {
+        public string Camera { get; set; }
+
+        public string Headphones { get; set; }
+
+        public string Phone { get; set; }
+        
+        public string Powerbank { get; set; }
+
+        public string  Headlamp{ get; set; }
+    }
+}
