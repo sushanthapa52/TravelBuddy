7c3e8963bc1ae219d0e5b942a33f747f6e1dc35d
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-08-14 20:40:36 -0400

    displaychecklist merged into main and fixed error

--- ViewModel/ExistingChecklistVIewModel.cs
+++ ViewModel/ExistingChecklistVIewModel.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
